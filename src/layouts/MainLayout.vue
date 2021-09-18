6089d03c528860d38a898a602768d4cea5f6a9ba
Meir Gabay <unfor19@gmail.com>
2021-09-19 00:53:45 +0300

    changed page title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> AWS Web-UI </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
